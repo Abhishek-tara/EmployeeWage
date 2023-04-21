004e25078899716eacde4e0efb5b0bce58f9b081
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-21 09:14:26 +0530

    Added Method to check employee

---
+++ EmployeeWage/CheckEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class CheckEmployee
+    {
+        public void EmployeeCheck()
+        {
+            int IS_FULLTIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULLTIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        
+        }
+
+
+
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckEmployee newEmp = new CheckEmployee();
+            newEmp.EmployeeCheck();
+        }
+    }
+}
