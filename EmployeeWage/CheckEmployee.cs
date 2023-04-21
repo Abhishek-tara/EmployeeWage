2b4a31df9b9b97a9876a6e0f0bd5b2daf6c1eb85
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-21 09:25:42 +0530

    Added Method to calculate Wage

--- EmployeeWage/CheckEmployee.cs
+++ EmployeeWage/CheckEmployee.cs
@@ -8,18 +8,25 @@ namespace EmployeeWage
 {
     public class CheckEmployee
     {
+        public int IS_FULLTIME = 1;
+        public int EMP_RATE_PER_HOUR = 20;
         public void EmployeeCheck()
         {
-            int IS_FULLTIME = 1;
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
 
             if (empCheck == IS_FULLTIME)
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             else
-                Console.WriteLine("Employee is absent");
-        
+                empHrs = 0;
+            
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage is: " + empWage);
+
         }
 
 
